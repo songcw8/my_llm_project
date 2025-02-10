25f051aeb9918d14ca155d59d22cbe911465a4d0
songcw8 <ben990158@gmail.com>
2025-02-10 15:16:13 +0900

    llm

--- docs/app.js
+++ docs/app.js
@@ -1,8 +1,26 @@
-async function main(){
-    async function handleCC(){
-        document.querySelector("#box").textContent = await (await fetch("http://127.0.0.1:3000")).text();
+async function main() {
+    async function hanldeCC(event) {
+      event.preventDefault(); // Form의 기본 submit 막아줘야하고...
+      const url = "http://127.0.0.1:3000";
+      const formData = new FormData(document.querySelector("#ccForm"));
+      const text = formData.get("text");
+      // console.log(text);
+      const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+          text,
+        }),
+        headers: {
+          "Content-Type": "Application/json",
+        },
+      });
+      const json = await response.json();
+  
+      document.querySelector("#box").textContent = JSON.stringify(json);
     }
-
-    document.querySelector("#ccBtn").addEventListener("click", handleCC);
-}
-document.addEventListener("DOMContentLoaded", main);
+  
+    //   document.querySelector("#ccBtn").addEventListener("click", hanldeCC);
+    document.querySelector("#ccForm").addEventListener("submit", hanldeCC);
+  }
+  
+  document.addEventListener("DOMContentLoaded", main);
